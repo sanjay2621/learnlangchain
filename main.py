8c694500c29863bc93f103a94d0caeb13368d573
sanjay2621 <sanjay.patel2621@gmail.com>
2025-09-06 12:40:34 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def main():
-    print("Jai Swaminarayan - Learn Langchain")
+    print("Jai Swaminarayan -> Learn Langchain")
 
 
 if __name__ == "__main__":
